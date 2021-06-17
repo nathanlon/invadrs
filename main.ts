a96c7c41c5fe02baa863ae3d0a3dbe52756239f3
Nathan O'Hanlon <nathan@prospertechnologies.com>
2021-06-17 16:15:45 +1200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 let x = 0
 let y = 0
+let bullet_y = 0
+let bullet_x = 0
 input.onButtonPressed(Button.A, function () {
     if (x > 0) {
         led.unplot(x, y)
@@ -13,6 +15,8 @@ input.onButtonPressed(Button.B, function () {
     }
 })
 input.onGesture(Gesture.Shake, function () {
+    bullet_y = 0
+    bullet_x = 0
     y = 4
     x = 2
     for (let x_pos = 0; x_pos <= 4; x_pos++) {
@@ -24,4 +28,17 @@ input.onGesture(Gesture.Shake, function () {
 })
 basic.forever(function () {
     led.plot(x, y)
+})
+basic.forever(function () {
+    for (let x_pos = 0; x_pos <= 4; x_pos++) {
+        for (let y_pos = 0; y_pos <= 3; y_pos++) {
+            led.unplot(x_pos, y_pos)
+        }
+    }
+    led.plot(bullet_x, bullet_y)
+    bullet_x += 1
+    basic.pause(1000)
+    if (bullet_x == 5) {
+        bullet_x = 0
+    }
 })
